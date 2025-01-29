78488f19ba410d35b54d2c3fa16fed564765ef5a
Shivam Kumar Sah <shivam_081bct039@tcioe.edu.np>
2025-01-29 08:35:45 +0545

    adding multiplication on my first report

---
+++ multiply.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a,b,m;
+    printf("enter your numbers\n");
+    scanf("%d%d",&a,&b);
+    m=a*b;
+    printf(" the multiplication of two numbers is %d",m);
+    return 0;
+    
+}
