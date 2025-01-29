43c484e237953aa470f71d0eb7f83f614ee925e5
Shivam Kumar Sah <shivam_081bct039@tcioe.edu.np>
2025-01-29 07:55:58 +0545

    initial commit

---
+++ sum.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a,b,s;
+    printf("enter your two numbers\n");
+    scanf("%d%d",&a,&b);
+    s=a+b;
+    printf("the sum is %d",s);
+    return 0;
+    
+
+}
